8b1cbeb25414c03bbbeba0139b310b0e0e83eeb9
Dey11 <shreyandey11@gmail.com>
2024-03-22 10:52:30 +0530

    move database connection to separate directory

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,40 +1,36 @@
-import express, { Request, Response } from 'express';
-import mongoose from 'mongoose';
-import cors from 'cors'
-import userRoutes from './routes/userRoutes';
+import express, { Request, Response } from "express";
+import mongoose from "mongoose";
+import cors from "cors";
+import userRoutes from "./routes/userRoutes";
+import { connect } from "http2";
+import connecttoMongoDB from "./db/index";
+
 // Create an Express application
 const app = express();
-require("dotenv").config()
-const PORT = process.env.PORT;
-const mongo_url = process.env.MONGO_URL
+require("dotenv").config();
+const PORT = process.env.PORT || 3000;
+const mongo_url = process.env.MONGO_URL;
 
 //middleware to enable CORS
-app.use(cors())
+app.use(cors());
 
 // Middleware to parse JSON bodies
 app.use(express.json());
-app.use(express.urlencoded({extended:true}))
-app.use(cors({credentials:true,origin:"http://localhost:3000"}))
+app.use(express.urlencoded({ extended: true }));
+app.use(cors({ credentials: true, origin: "http://localhost:3000" }));
 
 //Mount the routes.
-app.use('/api/users',userRoutes)
+app.use("/api/users", userRoutes);
 
 // Define routes
-app.get('/', (req: Request, res: Response) => {
+app.get("/", (req: Request, res: Response) => {
   res.send("Helloo world this is my server .");
 });
 
+// Connect to MongoDB
+connecttoMongoDB();
 
 // Start the server
 app.listen(PORT, async () => {
   console.log(`Server is running on http://localhost:${PORT}`);
-  try {
-    await mongoose.connect(mongo_url as string)
-    console.log("DataBase is successfully connected.")
-    
-  } catch (error) {
-    console.log("Error to connect Database.")
-    
-  }
- 
 });
